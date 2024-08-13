96e896c5c0c39f3d2499c876abbdc14d096a141c
Michał Lech <rootx@rootxnet.com>
2024-08-14 01:58:11 +0200

    Add creation/modification tracking to base model

--- api/contrib/models.py
+++ api/contrib/models.py
@@ -8,5 +8,9 @@ class BaseModel(models.Model):
         primary_key=True, unique=True, default=uuid.uuid4, editable=False
     )
 
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True, null=True, blank=True)
+
     class Meta:
         abstract = True
+        ordering = ["date_added"]
